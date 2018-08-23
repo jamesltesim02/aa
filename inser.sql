443c0e3a6672625c75fc0c6f04a40219cf6d86aa
jamesltesim02 <42630108+jamesltesim02@users.noreply.github.com>
2018-08-23 14:34:28 +0800

    Create inser.sql

---
+++ inser.sql
@@ -0,0 +1,2 @@
+insert into users(id, region, phone, nickname, passwordHash, passwordSalt, rongCloudToken, createdAt, updatedAt)
+values(1, '86', '18832145632', 'aaa', '229ea00e4d1a6bd3373067817df7f9db1cfd8341', '1000', '', now(), now())
